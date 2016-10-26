baf9b2f8655539e89a81de48cba774594ce4bea6
RyoBamboo <itakedaka@gmail.com>
2016-10-26 12:57:40 +0900

    カスタムUITextFieldの角丸と影を設定

--- omotenashiApp/CustomUITextField.swift
+++ omotenashiApp/CustomUITextField.swift
@@ -17,6 +17,14 @@ class CustomUITextField: UITextField {
       self.leftView = UIView(frame: CGRect(x: 0,y: 0,width: 10,height: 5))
       self.leftView?.backgroundColor = UIColor.clearColor()
       self.leftViewMode = UITextFieldViewMode.Always
+      
+      // 角丸設定
+      self.layer.cornerRadius = 4.0
+      
+      // 影設定
+      self.layer.shadowOpacity = 0.5
+      self.layer.masksToBounds = false
+      
     }
  
 
