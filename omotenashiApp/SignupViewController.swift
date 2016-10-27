ba3946eb2e4dc57242d882b5c0beea57fa21eaff
RyoBamboo <itakedaka@gmail.com>
2016-10-27 16:27:51 +0900

    サインアップ関数をAuthenticationクラスに移動

--- omotenashiApp/SignupViewController.swift
+++ omotenashiApp/SignupViewController.swift
@@ -12,38 +12,7 @@ class SignupViewController: UIViewController {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
-    // POSTでAPIを叩く
-    let url = NSURL(string:"http:omotenashi.prodrb.com/api/signup.php")
-    let request = NSMutableURLRequest(URL: url!)
-    let str = "name=test4&email=test4@gmail.com&password=test4&img=test4&languages=japanese,english&nationality=japan"
-    let strData = str.dataUsingEncoding(NSUTF8StringEncoding)
-    request.HTTPMethod = "POST"
-    request.HTTPBody = strData
-    request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData
-    request.timeoutInterval = 10.0
-    
-    var response: NSURLResponse?
-    do {
-      
-      // MEMO:NSURLConnectionは今後廃止されるのでNSURLSessionで書き直す必要あり
-      let data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response)
-      /*
-       let userInfo:NSDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
-       
-       if userInfo != false {
-       
-       // サインインできたらユーザ情報をUserDefaultsに辞書型で保存1
-       let userDefault = NSUserDefaults.standardUserDefaults()
-       userDefault.setObject(userInfo, forKey: "userInfo") // ユーザ情報を保存
-       userDefault.setObject(true, forKey: "isSignin") // サインイン情報を保存
-       userDefault.synchronize()
-       
-       
-       }
-       */
-    } catch (let e) {
-      print(e)
-    }
+
   }
   
   override func didReceiveMemoryWarning() {
