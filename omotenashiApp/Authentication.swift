0dd24a4b811ededb61ffba997858eb28fe3eb99b
RyoBamboo <itakedaka@gmail.com>
2016-10-30 16:03:16 +0900

    サインアップ処理のリファクタリング

--- omotenashiApp/Authentication.swift
+++ omotenashiApp/Authentication.swift
@@ -2,7 +2,6 @@
 //  Authentication.swift
 //  omotenashiApp
 //
-//  Created by 竹之下遼 on 2016/10/26.
 //  Copyright © 2016年 TMT. All rights reserved.
 //
 
@@ -12,10 +11,19 @@ import UIKit
 class Authentication {
   
   class func signup(name: String, email: String, password: String, img: String, languages: String, nationality: String)->Bool {
+    
     // POSTでAPIを叩く
     let url = NSURL(string:Const.apiSignupUrl)
     let request = NSMutableURLRequest(URL: url!)
-    let str = "name=test4&email=test4@gmail.com&password=test4&img=test4&languages=japanese,english&nationality=japan"
+
+    // パラメータの作成
+    var str = "name="+name+"&"
+        str = str + "email="+email+"&"
+        str = str + "password="+password+"&"
+        str = str + "img="+img+"&"
+        str = str + "languages="+languages+"&"
+        str = str + "nationality="+nationality
+    
     let strData = str.dataUsingEncoding(NSUTF8StringEncoding)
     request.HTTPMethod = "POST"
     request.HTTPBody = strData
@@ -27,7 +35,10 @@ class Authentication {
       
       // MEMO:NSURLConnectionは今後廃止されるのでNSURLSessionで書き直す必要あり
       let data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response)
-      return true
+      if (data != false) {
+        return true
+      }
+      
     } catch (let e) {
       print(e)
     }
@@ -95,6 +106,8 @@ class Authentication {
     let signInViewController = storyboard.instantiateViewControllerWithIdentifier("signin") as! SignInViewController
 
     
+    // MEMO: これだとサインアウトボタンを押すたびにViewControllerが無限に生成されて
+    //        メモリを圧迫するので要リファクタリング
     var tc = UIApplication.sharedApplication().keyWindow?.rootViewController;
     tc?.dismissViewControllerAnimated(true, completion: nil)
     while ((tc!.presentedViewController) != nil) {
--- omotenashiApp/SignupViewController.swift
+++ omotenashiApp/SignupViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 class SignupViewController: UIViewController {
   
   @IBOutlet weak var nameTextField: CustomUITextField!
+  @IBOutlet weak var emailTextField: CustomUITextField!
   @IBOutlet weak var passwordTextField: CustomUITextField!
   
     override func viewDidLoad() {
@@ -24,8 +25,22 @@ class SignupViewController: UIViewController {
   }
   
   @IBAction func signupButton(sender: AnyObject) {
-    let nameStr = self.nameTextField.text
-    let passwordStr = self.passwordTextField.text
+    let nameStr = self.nameTextField.text!
+    let emailStr = self.emailTextField.text!
+    let passwordStr = self.passwordTextField.text!
+    let imgStr = "test"
+    let languagesStr = "test"
+    let nationalityStr = "test"
+
+    
+    // サインアップ処理
+    let isSignup: Bool = Authentication.signup(nameStr, email: emailStr, password: passwordStr, img: imgStr, languages: languagesStr, nationality: nationalityStr)
+    // サインアップできたらメイン画面へ遷移
+    if (isSignup == true) {
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      let viewController = storyboard.instantiateInitialViewController()! as UIViewController
+      self.presentViewController(viewController, animated: true, completion: nil)
+    }
   }
   
   /*
