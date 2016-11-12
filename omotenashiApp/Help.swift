85b0007b4b411b722ce6211aaf518feb44521445
RyoBamboo <itakedaka@gmail.com>
2016-11-12 12:57:16 +0900

    不要な部分の削除

--- omotenashiApp/Help.swift
+++ omotenashiApp/Help.swift
@@ -105,26 +105,6 @@ class Help {
                     helpInfo["notifiedHelpIdList"] = notifiedHelpIdList
                     userDefault.setObject(helpInfo, forKey: "helpInfo")
                 }
-//                
-//                
-//               
-//                
-//                //ローカル通知
-//                let notification = UILocalNotification()
-//                //ロック中にスライドで〜〜のところの文字
-//                notification.alertAction = "アプリを開く"
-//                //通知の本文
-//                notification.alertBody = String(helpRequest.distance) + "m先で困っている人がいます！！"
-//                //通知される時間（とりあえず10秒後に設定）
-//                notification.fireDate = NSDate(timeIntervalSinceNow:0.1)
-//                //通知音
-//                notification.soundName = UILocalNotificationDefaultSoundName
-//                //アインコンバッジの数字
-//                notification.applicationIconBadgeNumber = 1
-//                //通知を識別するID
-//                notification.userInfo = ["notifyID":"gohan"]
-//                //通知をスケジューリング
-//                UIApplication.sharedApplication().scheduleLocalNotification(notification)
                 
                 
             } catch let error {
