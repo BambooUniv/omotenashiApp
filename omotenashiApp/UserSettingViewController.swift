c4c5458ee1bb81448a314ae6059ed0c242527551
RyoBamboo <itakedaka@gmail.com>
2016-11-14 20:15:19 +0900

    設定画面の作成

--- omotenashiApp/UserSettingViewController.swift
+++ omotenashiApp/UserSettingViewController.swift
@@ -21,6 +21,8 @@ class UserSettingViewController: UIViewController {
     
     
     
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var pointButton: UIButton!
     
     
     
@@ -28,19 +30,25 @@ class UserSettingViewController: UIViewController {
         super.viewDidLoad()
         
         // 設定画面を表示する
-        pointView.hidden = false
-        settingView.hidden = true
+        pointView.hidden = true
+        settingView.hidden = false
+        
+        // ボタンの装飾
+        settingButton.backgroundColor = UIColor(red: CGFloat(156)/255.0, green: CGFloat(141)/255.0, blue: CGFloat(42)/255.0, alpha: 1.0)
+        settingButton.layer.cornerRadius = 7
+        pointButton.backgroundColor = UIColor(red: CGFloat(156)/255.0, green: CGFloat(141)/255.0, blue: CGFloat(42)/255.0, alpha: 1.0)
+        pointButton.layer.cornerRadius = 7
         
     }
     
     
     @IBAction func showSettingButton(sender: AnyObject) {
-        pointView.hidden = false
-        settingView.hidden = true
+        pointView.hidden = true
+        settingView.hidden = false
     }
     
     @IBAction func showPointButton(sender: AnyObject) {
-        pointView.hidden = true
-        settingView.hidden = false
+        pointView.hidden = false
+        settingView.hidden = true
     }
 }
