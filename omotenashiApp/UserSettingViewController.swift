5b86b1782b91a6e0825193e9cd2e877037332bb8
RyoBamboo <itakedaka@gmail.com>
2016-11-14 19:42:44 +0900

    設定画面の作成

--- omotenashiApp/UserSettingViewController.swift
+++ omotenashiApp/UserSettingViewController.swift
@@ -7,15 +7,40 @@
 import Foundation
 import UIKit
 
-class UserSettingViewController: UIViewController{
+class UserSettingViewController: UIViewController {
+    
+    // 画面を切り替えるTabBar
+    
+    // ポイント画面関する要素
+    @IBOutlet weak var pointView: UIView!
+    
+    
+    
+    // 設定画面に関する要素
+    @IBOutlet weak var settingView: UIView!
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        print("test")
-        
+        // 設定画面を表示する
+        pointView.hidden = false
+        settingView.hidden = true
         
     }
     
+    
+    @IBAction func showSettingButton(sender: AnyObject) {
+        pointView.hidden = false
+        settingView.hidden = true
+    }
+    
+    @IBAction func showPointButton(sender: AnyObject) {
+        pointView.hidden = true
+        settingView.hidden = false
+    }
 }
