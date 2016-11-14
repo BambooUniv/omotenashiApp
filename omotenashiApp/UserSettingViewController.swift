61d95f80c19d805a98048cc798388a6c61b7b84d
RyoBamboo <itakedaka@gmail.com>
2016-11-14 12:55:05 +0900

    UITabbar追加

--- omotenashiApp/UserSettingViewController.swift
+++ omotenashiApp/UserSettingViewController.swift
@@ -5,3 +5,17 @@
 //
 
 import Foundation
+import UIKit
+
+class UserSettingViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        print("test")
+        
+        
+    }
+    
+}
