19203d44d7fc718237507b82d7aaeb6c43db36c2
RyoBamboo <itakedaka@gmail.com>
2016-11-15 16:19:58 +0900

    クラス追加したのでプッシュ

---
+++ omotenashiApp/MyTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MyTabBarController.swift
+//  omotenashiApp
+//
+//
+
+import Foundation
+import UIKit
+
+class MyTabBarController :UITabBarController {
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        self.tabBar.barTintColor = UIColor.brownColor()
+    }
+}
